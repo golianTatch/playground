c54e8fe6eb239520126f3b293d48bd475b501035
Tatch <tatsuya.hirase@gmail.com>
2016-01-03 09:18:28 +0900

    change username

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -5,6 +5,8 @@ import nonogram.NonogramLogic;
 public class Driver {
 
 	public static void main(String[] args) {
+		//why username does not change?
+		
 //		PalindromeFounder pf = new PalindromeFounder();
 //		System.out.println(pf.toNBased(21, 7, ""));
 		
